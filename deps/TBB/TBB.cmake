8b69b1e599b5b22d4e456b8b2e67ea26e8e7c52d
fr3ak2402 <fr3ak2402@gmail.com>
2023-08-01 17:52:52 +0200

    Update TBB.cmake

--- deps/TBB/TBB.cmake
+++ deps/TBB/TBB.cmake
@@ -1,7 +1,7 @@
 bambustudio_add_cmake_project(
     TBB
-    URL "https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.9.0.zip"
-    URL_HASH SHA256=fcebb93cb9f7e882f62cd351b1c093dbefdcae04b616227dc716b0a5efa9e8ab
+    URL "https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.10.0.zip"
+    URL_HASH SHA256=78fb7bb29b415f53de21a68c4fdf97de8ae035090d9ee9caa221e32c6e79567c
     #PATCH_COMMAND ${PATCH_CMD} ${CMAKE_CURRENT_LIST_DIR}/0001-TBB-GCC13.patch
     CMAKE_ARGS          
         -DTBB_BUILD_SHARED=OFF
