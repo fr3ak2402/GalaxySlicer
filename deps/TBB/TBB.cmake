d8adeb5368824e95a04b337ec217a6a89c3b763e
fr3ak2402 <fr3ak2402@gmail.com>
2023-08-02 15:49:05 +0200

    Update TBB.cmake

--- deps/TBB/TBB.cmake
+++ deps/TBB/TBB.cmake
@@ -1,7 +1,17 @@
+
+# Use oneTBB v2021.10.0 for Windows and macOS to fully support hybrid CPUs.
+if (WIN32)
+	set(_oneTBB_url "https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.10.0.zip")
+	set(_oneTBB_hash 78fb7bb29b415f53de21a68c4fdf97de8ae035090d9ee9caa221e32c6e79567c)
+else()
+	set(_oneTBB_url "https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.10.0.zip")
+	set(_oneTBB_hash 78fb7bb29b415f53de21a68c4fdf97de8ae035090d9ee9caa221e32c6e79567c)
+endif()
+
 bambustudio_add_cmake_project(
     TBB
-    URL "https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.10.0.zip"
-    URL_HASH SHA256=78fb7bb29b415f53de21a68c4fdf97de8ae035090d9ee9caa221e32c6e79567c
+    URL ${_oneTBB_url}
+    URL_HASH SHA256=${_oneTBB_hash}
     #PATCH_COMMAND ${PATCH_CMD} ${CMAKE_CURRENT_LIST_DIR}/0001-TBB-GCC13.patch
     CMAKE_ARGS          
         -DTBB_BUILD_SHARED=OFF
