eaf02f4483aa4bc074ccbe1d0f1e8654bfcd526b
Heiko Liebscher <hliebscher@idn.de>
2023-08-07 09:53:24 +0200

    script update

---
+++ pack_deps_arm64.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+// shellscript for creating dependencies for arm64
+
+
+cd /Users/runner/work/GalaxySlicer/GalaxySlicer/deps/build_arm64/
+tar cfvz /Users/runner/work/GalaxySlicer/GalaxySlicer_dep_mac_arm64.tar.gz GalaxySlicer_dep_arm64 
