da9206d334ac3687cfd898114384e49db6262082
fr3ak2402 <96239814+fr3ak2402@users.noreply.github.com>
2023-10-06 09:47:42 +0200

    Update postprocessor.py

--- resources/profiles/Creality/postprocessor/postprocessor.py
+++ resources/profiles/Creality/postprocessor/postprocessor.py
@@ -13,27 +13,27 @@ def update_signature_and_replace(source_file):
         content = f.read()
     
     # Extract the printer model information from the content
-    printer_model_line = re.search(r';printer_model\s*=\s*(.*)', content)
-    printer_model = printer_model_line.group(1).strip()
+    #printer_model_line = re.search(r';printer_model\s*=\s*(.*)', content)
+    #printer_model = printer_model_line.group(1).strip()
     
     # Check if the printer model is either "Creality K1" or "Creality K1 Max"
-    if printer_model == "Creality K1" or printer_model == "Creality K1 Max":
+    #if printer_model == "Creality K1" or printer_model == "Creality K1 Max":
     
-        # Replace '; thumbnail begin' and '; thumbnail end' with '; png begin' and '; png end'
-        content = content.replace('; thumbnail begin', '; png begin').replace('; thumbnail end', '; png end')
+    # Replace '; thumbnail begin' and '; thumbnail end' with '; png begin' and '; png end'
+    content = content.replace('; thumbnail begin', '; png begin').replace('; thumbnail end', '; png end')
     
-        # Store lines with '; thumbnail begin'
-        thumbnail_lines = re.findall(r'; png begin(.+?);', content, re.DOTALL)
+    # Store lines with '; thumbnail begin'
+    thumbnail_lines = re.findall(r'; png begin(.+?);', content, re.DOTALL)
 
-        # Edit the stored thumbnail lines by replacing 'x' with '*'
-        edited_thumbnail_lines = [line.replace('x', '*') for line in thumbnail_lines]
+    # Edit the stored thumbnail lines by replacing 'x' with '*'
+    edited_thumbnail_lines = [line.replace('x', '*') for line in thumbnail_lines]
     
-        # Replace the original thumbnail lines with the edited lines
-        for i in range(len(thumbnail_lines)):
-            content = content.replace(thumbnail_lines[i], edited_thumbnail_lines[i])
+    # Replace the original thumbnail lines with the edited lines
+    for i in range(len(thumbnail_lines)):
+        content = content.replace(thumbnail_lines[i], edited_thumbnail_lines[i])
 
-        # Update M106 commands: Replace M106 P3 with M106 P1 (commented out as it's not currently used)
-        # content = re.sub(r'M106 P3', r'M106 P1', content)
+    # Update M106 commands: Replace M106 P3 with M106 P1 (commented out as it's not currently used)
+    # content = re.sub(r'M106 P3', r'M106 P1', content)
 
     # Write the updated content back to the same file
     with open(source_file, "w") as of:
