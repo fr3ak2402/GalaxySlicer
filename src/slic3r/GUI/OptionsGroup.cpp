7d38006be0e3ffe8c07a7a37228fddd8863289ac
Heiko Liebscher <hliebscher@idn.de>
2023-10-22 12:22:21 +0200

    fix wiki

--- src/slic3r/GUI/OptionsGroup.cpp
+++ src/slic3r/GUI/OptionsGroup.cpp
@@ -1259,7 +1259,7 @@ wxString OptionsGroup::get_url(const std::string& path_end)
         return wxString::Format(L"https://wiki.bambulab.com/%s/software/bambu-studio/%s", L"en", str);
     }
     // Orca: point to sf wiki for seam parameters
-    return wxString::Format(L"https://github.com/SoftFever/OrcaSlicer/wiki/Print-settings/%s", from_u8(path_end));
+    //return wxString::Format(L"https://github.com/SoftFever/OrcaSlicer/wiki/Print-settings/%s", from_u8(path_end));
 
 }
 
