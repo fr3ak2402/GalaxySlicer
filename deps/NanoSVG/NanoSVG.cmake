ea0bcfcf0a797f1672fde854db89e9720c703f05
Heiko Liebscher <hliebscher@idn.de>
2023-12-11 10:15:27 +0100

    fix add_to_galaxyslicer_cmake_project

--- deps/NanoSVG/NanoSVG.cmake
+++ deps/NanoSVG/NanoSVG.cmake
@@ -3,7 +3,7 @@
 # for rasterizing svg files from their original size to a squared power of two texture on Windows systems using
 # AMD Radeon graphics cards
 
-orcaslicer_add_cmake_project(NanoSVG
+add_to_galaxyslicer_cmake_project(NanoSVG
         URL https://github.com/fltk/nanosvg/archive/abcd277ea45e9098bed752cf9c6875b533c0892f.zip
         URL_HASH SHA256=e859938fbaee4b351bd8a8b3d3c7a75b40c36885ce00b73faa1ce0b98aa0ad34
 )
