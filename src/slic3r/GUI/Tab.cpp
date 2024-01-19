d7e9a1992c8bccfe2593c56dcba0160d16d5c313
Heiko Liebscher <hliebscher@idn.de>
2024-01-19 12:01:24 +0100

    [Feature] Enabled gap fill algorithm for all solid fill types (#3412)
    * ENH: Enabled gap fill algorithm for all solid fill types
    
    * Made gap fill an option & refactored code into its own method
    
    * Code comment updates
    
    * Converted gap fill to enum and control filter out gap fill in the UI
    
    * Update label for consistency
    
    * Spelling mistake

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -2008,6 +2008,7 @@ void TabPrint::build()
         optgroup->append_single_option_line("infill_anchor");
         optgroup->append_single_option_line("infill_anchor_max");
         optgroup->append_single_option_line("internal_solid_infill_pattern");
+        optgroup->append_single_option_line("gap_fill_target");
         optgroup->append_single_option_line("filter_out_gap_fill");
 
         optgroup = page->new_optgroup(L("Advanced"), L"param_advanced");
